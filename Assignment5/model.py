12f42192a7e36cdd9269be327e7e80250f7cb2b0
Le Van Duc <duclv@kdb.snu.ac.kr>
2019-11-25 13:41:01 +0900

    Assignment5

---
+++ Assignment5/model.py
@@ -0,0 +1,27 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
+import distutils.version
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
+import distutils.version
+
+
+class LSTMPolicy(object):
+    def __init__(self, ob_space, ac_space):
+        self.x = x = tf.placeholder(tf.float32, [None] + list(ob_space))
+        """
+        Implement your code here
+        """
+        
+    def act(self, ob, c, h):
+        """
+        Implement your code here
+        """
+
+    def value(self, ob, c, h):
+        """
+        Implement your code here
+        """
+
